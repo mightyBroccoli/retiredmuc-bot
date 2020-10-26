a109b226bf6503584b4dea5a4203141e44338b85
nico <nico@magicbroccoli.de>
2020-10-26 19:23:22 +0100

    Initial working release
    XMPP retired MUC Bot
    
    This Bot connects to a chosen MUC to inform connecting users of
    the new/ different MUC they should connect to.

---
+++ config.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+from pathlib import Path
+
+from ruamel.yaml import YAML
+from ruamel.yaml.parser import ParserError
+from ruamel.yaml.scanner import ScannerError
+
+
+class Config:
+    def __init__(self):
+        # class variables
+        self.content = None
+
+        # select config file
+        self.conf_file = Path("config.yml")
+        if not Path.exists(self.conf_file):
+            self.conf_file = Path("/etc/retiredmuc-bot.yml")
+
+        # read config file
+        self._read()
+
+    def _read(self):
+        """init the config object with this method"""
+        self._check()
+
+        # open file as an iostream
+        with open(self.conf_file, "r", encoding="utf-8") as f:
+            try:
+                self.content = YAML(typ="safe").load(f)
+
+            # catch decoding errors
+            except (ParserError, ScannerError) as err:
+                print(err, file=sys.stderr)
+                exit(1)
+
+    def _check(self):
+        """internal method to check if the config file exists"""
+        try:
+            # if file is present continue
+            if self.conf_file.exists():
+                return
+
+            # if not create a blank file
+            else:
+                self.conf_file.touch(mode=0o640)
+
+        # catch permission exceptions as this tries to write to /etc/
+        except PermissionError as err:
+            print(err, file=sys.stderr)
+            sys.exit(err.errno)
+
+    def get(
+        self, key: str = None, default: (str, int) = None
+    ) -> (dict, str, int, None):
+        """method to retrieve the whole config data, a single value or the optional default value"""
+        # if a special key is request, return only that value
+        if key is not None:
+
+            # safety measure
+            if key in self.content:
+                return self.content[key]
+
+            # if a default value is given return that
+            if default is not None:
+                return default
+
+            # if the key isn't part if self.content return None
+            else:
+                return None
+
+        # else return everything
+        return self.content
---
+++ main.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+
+from slixmpp import ClientXMPP
+
+from config import Config
+
+
+class RetiredMucBot(ClientXMPP):
+    def __init__(self, jid, password, nick, config):
+        ClientXMPP.__init__(self, jid, password)
+        self.use_message_ids = True
+        self.use_ssl = True
+
+        # passthrough the config obj
+        self.config = config
+        
+        self.rooms = None
+        self.nick = nick
+        self.messages = self.config.get("messages")
+
+        # feature config
+        self.functions = self.config.get("features")
+
+        # session start disconnect events
+        self.add_event_handler("session_start", self.start_session)
+        self.add_event_handler("disconnected", self.reconnect_session)
+
+        # register receive handler for both groupchat and normal message events
+        self.add_event_handler("message", self.message)
+
+    def reconnect_session(self, event):
+        """
+        method to handle disconnects / session drops
+        """
+        self.connect()
+
+    def start_session(self, event):
+        """
+        session start
+        """
+        self.send_presence()
+        self.join_rooms()
+
+    def join_rooms(self):
+        """
+        method to join configured rooms and register their response handlers
+        """
+        self.rooms = self.config.get("rooms")
+
+        if self.rooms is not None:
+            for room in self.rooms:
+                self.add_event_handler(f"muc::{room}::got_online", self.notify_user)
+                self.plugin["xep_0045"].join_muc(room, self.nick, wait=True)
+
+    def message(self, msg):
+        """
+        method to handle incoming chat, normal messages
+        :param msg: incoming msg object
+        """
+
+        # do not process our own messages
+        ourself = self.plugin['xep_0045'].get_our_jid_in_room(msg.get_mucroom())
+        if msg['from'] == ourself:
+            return
+
+        # ever other messages will be answered statically
+        if msg['type'] in ('normal', 'chat'):
+            self.send_message(
+                mto=msg['from'],
+                mbody=self.messages['direct_msg'].format(nick=self.nick),
+                mtype=msg['type'],
+            )
+
+    def notify_user(self, presence):
+        """
+        method to compose the redirect action
+        :param presence: incoming user presence object
+        """
+        user_nick = presence["muc"]["nick"]
+
+        # catch empty user_nicks slixmpp does that for some reason
+        if user_nick == "":
+            return
+
+        # handle all incoming user presences, this may cause duplicates
+        if user_nick != self.nick:
+            new_room = self.rooms[presence.get_from().bare]
+            self.send_message(
+                mto=presence.get_from().bare,
+                mbody=self.messages['grp_msg'].format(user_nick=user_nick, new_room=new_room),
+                mtype="groupchat",
+            )
+
+            if self.functions['direct_invite']:
+                jid = presence["muc"].get_jid().bare
+                self.invite_user(jid, new_room)
+
+    def invite_user(self, jid, room):
+        """
+        method to invite the user to the new room
+        :param jid: jid to invite
+        :param room: room to which the jid should be invited
+        """
+        self.plugin["xep_0045"].invite(
+            room=jid,
+            jid=room,
+            reason='redirection due to retirement',
+        )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    # init config obj
+    config = Config()
+
+    # config
+    login = config.get("login", default=None)
+
+    # init the bot and register used slixmpp plugins
+    xmpp = RetiredMucBot(login["jid"], login["password"], login["nick"], config)
+    #xmpp.register_plugin("xep_0004")  # Data Forms
+    xmpp.register_plugin("xep_0030")  # Service Discovery
+    xmpp.register_plugin("xep_0045")  # Multi-User Chat
+    xmpp.register_plugin("xep_0085")  # Chat State Notifications
+    xmpp.register_plugin('xep_0092')  # Software Version
+    xmpp.register_plugin("xep_0199")  # XMPP Ping
+    xmpp.register_plugin("xep_0249")  # Direct MUC Invite
+
+    # connect and start receiving stanzas
+    xmpp.connect()
+    xmpp.process()
